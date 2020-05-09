4accf810ed66c53accfd2c7dd243cf1d819deff8
Barry <barry@tunetheweb.com>
2020-05-09 15:59:31 +0100

    Comment out to test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const generateMarkdownReport = require("./github-markdown");
-const processImages = require("./image-processing");
-const createComment = require("./github-pr-comment");
-const createCommit = require("./github-commit");
+//const generateMarkdownReport = require("./github-markdown");
+//const processImages = require("./image-processing");
+//const createComment = require("./github-pr-comment");
+//const createCommit = require("./github-commit");
 
 const run = async () => {
   console.log("->> Locating images…");
 
-  const results = await processImages();
+  /*const results = await processImages();
 
   const optimisedImages = results.images.filter(
     img => img.compressionWasSignificant
@@ -16,10 +16,11 @@ const run = async () => {
   if (!optimisedImages.length) {
     console.log("::warning:: Nothing left to optimise. Stopping…");
     return;
-  }
+  }*/
 
   console.log("->> Generating markdown…");
-  const markdown = await generateMarkdownReport(results);
+  //const markdown = await generateMarkdownReport(results);
+  markdown = "Test output\nTest Line2\n\nTest line4"
   console.log("::set-output name=calibre_markdown::" + markdown)
 
   // If compress only mode, then we're done
@@ -29,10 +30,10 @@ const run = async () => {
   }
 
   console.log("->> Committing files…");
-  await createCommit(optimisedImages);
+  //await createCommit(optimisedImages);
 
   console.log("->> Leaving comment on PR…");
-  await createComment(markdown);
+  //await createComment(markdown);
 
   return results;
 };
